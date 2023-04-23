5c780f37008ca04db9cb5fe315cbd76237f2fe4d
HalhalZineb1 <105636006+HalhalZineb1@users.noreply.github.com>
2023-04-23 23:43:18 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,16 +1,12 @@
  <?php
-
+$data = "some data to log";
+echo "<script>console.log('" . $data . "');</script>";
     include('connexion.php');
-console.log("Message to log2");
-
     $req="INSERT INTO utilisateur (idUser,NOM, EMAIL, ADRESSE, PWD) VALUES ("1",?, ?, ?, ?)";
     $CNE=$_POST['login'];
     $Nom=$_POST['nom'];
     $Adresse=$_POST['adresse'];
     $PW=$_POST['pwd'];
-                          
-console.log("Message to log");
-
 
     $reslt=$dbh->prepare($req);
     $reslt->execute(array($CNE,$Nom,$Adresse,$PW));
