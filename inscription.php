56c1d49af46a3eba56eeb2e0e588a4129c098b32
HalhalZineb1 <105636006+HalhalZineb1@users.noreply.github.com>
2023-04-23 23:50:12 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,6 +1,6 @@
  <?php
     include('connexion.php');
-    $req="INSERT INTO utilisateur (NOM, EMAIL, ADRESSE, PWD) VALUES (?, ?, ?, ?)";
+    $req="INSERT INTO utilisateur (idUser,NOM, EMAIL, ADRESSE, PWD) VALUES ('1','yass', 'test', 'test', 'test');";
     $CNE=$_POST['login'];
     $Nom=$_POST['nom'];
     $Adresse=$_POST['adresse'];
