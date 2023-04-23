af11255af255d23de03da7b4399306c2910057cf
HalhalZineb1 <105636006+HalhalZineb1@users.noreply.github.com>
2023-04-23 22:39:39 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -3,7 +3,7 @@ $db="mysql:host=localhost;dbname=aws";
 $login="root";
 $pw="";
 try{
-	$dbh = new PDO($db,$login,$pw);
+	$dbh = new PDO("sqlsrv:server = tcp:azuresqlserverzh.database.windows.net,1433; Database = aws", "azuresqlserver", "{your_password_here}");
 }
 catch (PDOException $e){
 	print "Error!:".$e->getmessage()."<br/>";
