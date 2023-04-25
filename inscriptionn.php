3b17116603470390f171c82ed3463672f2486867
HalhalZineb1 <105636006+HalhalZineb1@users.noreply.github.com>
2023-04-26 00:51:29 +0200

    Update inscriptionn.php

--- inscriptionn.php
+++ inscriptionn.php
@@ -7,27 +7,113 @@
     <title>Registration Form</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-  <style>  body {
-    background: linear-gradient(#141e30, #243b55);       
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  <style>  
+      @property --rotate {
+  syntax: "<angle>";
+  initial-value: 132deg;
+  inherits: false;
 }
 
-  .container {
-  max-width: 400px;
+:root {
+  --card-height: 65vh;
+  --card-width: calc(var(--card-height) / 1.5);
+}
+
+
+body {
+  min-height: 100vh;
+  background: #212534;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+  box-sizing: border-box;
+}
+
+
+.card {
+  background: #191c29;
+  width: var(--card-width);
+  height: var(--card-height);
+  padding: 3px;
+  position: relative;
+  border-radius: 6px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  display: flex;
+  font-size: 1.5em;
+  color: rgb(88 199 250 / 0%);
+  cursor: pointer;
+  font-family: cursive;
+}
+
+.card:hover {
+  color: rgb(88 199 250 / 100%);
+  transition: color 1s;
+}
+.card:hover:before, .card:hover:after {
+  animation: none;
+  opacity: 0;
+}
+
+
+.card::before {
+  content: "";
+  width: 104%;
+  height: 102%;
+  border-radius: 8px;
+  background-image: linear-gradient(
+    var(--rotate)
+    , #5ddcff, #3c67e3 43%, #4e00c2);
+    position: absolute;
+    z-index: -1;
+    top: -1%;
+    left: -2%;
+    animation: spin 2.5s linear infinite;
+}
+
+.card::after {
+  position: absolute;
+  content: "";
+  top: calc(var(--card-height) / 6);
+  left: 0;
+  right: 0;
+  z-index: -1;
+  height: 100%;
+  width: 100%;
   margin: 0 auto;
-  background-color: white;
-  padding: 20px;
-  border-radius: 10px;
+  transform: scale(0.8);
+  filter: blur(calc(var(--card-height) / 6));
+  background-image: linear-gradient(
+    var(--rotate)
+    , #5ddcff, #3c67e3 43%, #4e00c2);
+    opacity: 1;
+  transition: opacity .5s;
+  animation: spin 2.5s linear infinite;
+}
 
-  
+@keyframes spin {
+  0% {
+    --rotate: 0deg;
+  }
+  100% {
+    --rotate: 360deg;
+  }
+}
 
+a {
+  color: #212534;
+  text-decoration: none;
+  font-family: sans-serif;
+  font-weight: bold;
+  margin-top: 2rem;
 }
 </style>
 </head>
 <body>
+    
     <div class="container">
         
         <form action="inscription.php" method="post">
@@ -50,12 +136,14 @@
         </form>
         <div>
      
-        <div><p>Already Registered <a href="index.php"> <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-            Login Here</a></p></div>
+     
+     <div>
+        <div><p>Already Registered <a href="index.php">Login Here</a></p></div>
       </div>
     </div>
+	
+</div>
+
+<a href="acceuil.php" target="_blank">Acces direct</a>
 </body>
 </html>
