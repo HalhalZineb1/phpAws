01b69b44b39e94e25a6da8e071409d5eef727826
HalhalZineb1 <105636006+HalhalZineb1@users.noreply.github.com>
2023-04-25 18:39:26 +0200

    Update guide.php

--- pages/guide.php
+++ pages/guide.php
@@ -45,16 +45,16 @@
       <label for='step-1'>Étape initiale</label>
     </div>
     <div class='progress_inner__step'>
-      <label for='step-2'> Stockage Azure</label>
+      <label for='step-2'> Développement local</label>
     </div>
     <div class='progress_inner__step'>
-      <label for='step-3'> App Service</label>
+      <label for='step-3'> Configuration BDD</label>
     </div>
     <div class='progress_inner__step'>
-      <label for='step-4'>BDD</label>
+      <label for='step-4'>Déploiement continu</label>
     </div>
     <div class='progress_inner__step'>
-      <label for='step-5'>domaine&sécurité</label>
+      <label for='step-5'>Commit & Push</label>
     </div>
 	
     <input checked='checked' id='step-1' name='step' type='radio'>
@@ -66,10 +66,11 @@
     <div class='progress_inner__bar--set'></div>
     <div class='progress_inner__tabs'>
       <div class='tab tab-0'>
-        <h1>Créer et cloner le référentiel GitHub pour votre site web</h1>
+        <h1>Suivez ces étapes pour développer et déployer votre site web.<br> Créer et cloner le référentiel GitHub pour votre site web</h1>
         <p>
    	
-		Description : Dans cette étape, vous allez créer un référentiel GitHub pour votre site web et cloner ce référentiel sur votre ordinateur local. Le référentiel GitHub sera utilisé pour stocker votre code source et le déploiement continu de votre application sur Microsoft Azure.
+		Description : Dans cette étape, vous allez créer un référentiel GitHub pour votre site web et cloner ce référentiel sur votre ordinateur local.
+		<br>Le référentiel GitHub sera utilisé pour stocker votre code source et le déploiement continu de votre application sur Microsoft Azure.
 <br>
 		Instructions :
 <br>
@@ -89,38 +90,49 @@
       <div class='tab tab-1'>
         <h1>Installation de PHP et de la base de données SQL :</h1>
         <p>
-        
-
-Connectez-vous à votre instance EC2 via SSH.
-Installez Apache, PHP et MySQL ou MariaDB sur votre instance EC2.
-Configurez votre base de données SQL pour qu'elle accepte les connexions distantes et créez votre base de données et vos tables.
+		
+		Description : Dans cette étape, vous allez développer votre site web en utilisant PHP, JS et la base de données SQL sur votre ordinateur local.
+		<br>Cette étape comprend la création des pages web,
+		<br>la création et la configuration de la base de données, ainsi que l'ajout du code nécessaire pour les fonctionnalités d'inscription et de connexion.
+		<br>Créez les fichiers HTML, PHP, JS et CSS nécessaires pour votre site web.
+		Etapes: <br>
+<br>Configurez la base de données SQL en utilisant votre logiciel de gestion de bases de données préféré.
+<br>Créez les tables nécessaires pour votre site web en utilisant le langage SQL.
+<br>Ajoutez le code PHP pour la connexion à la base de données et pour le traitement des requêtes d'inscription et de connexion.
+<br>Testez votre site web en exécutant un serveur PHP local sur votre ordinateur.
 </p>
       </div>
       <div class='tab tab-2'>
-        <h1> Téléchargement de votre application web:</h1>
+        <h1> Configurer les paramètres de l'application web sur Azure pour qu'elle soit connectée à la base de données SQL:</h1>
         <p>
-
-
-Téléchargez votre application Web PHP sur votre instance EC2.
-Assurez-vous que tous les fichiers sont placés dans le répertoire racine d'Apache, généralement /var/www/html/.
+		Configurer les paramètres de l'application web sur Azure pour qu'elle soit connectée à la base de données SQL en utilisant l'onglet Configuration dans le portail Azure :
+<br>
+1.Connectez-vous au portail Azure et accédez à l'application web que vous avez créée précédemment.
+<br>2.Cliquez sur l'onglet Configuration dans le menu de gauche.
+<br>3.Cliquez sur le bouton "Ajouter une chaîne de connexion" et entrez les informations de votre base de données SQL, y compris le nom du serveur, le nom de la base de données, le nom d'utilisateur et le mot de passe.
+<br>4. Cliquez sur "OK" pour ajouter la chaîne de connexion.
 	</p>
       </div>
       <div class='tab tab-3'>
-        <h1>Configuration de votre application Web PHP :</h1>
+        <h1>Activer la fonctionnalité de déploiement continu avec GitHub comme centre de déploiement :</h1>
         <p>
-        
-
-Créez un fichier de configuration pour votre application Web PHP en définissant les informations de connexion à la base de données SQL, les variables d'environnement, etc.
-Configurez les fichiers de votre application Web PHP pour qu'ils fonctionnent correctement avec Apache.
+        Activez la fonctionnalité de déploiement continu (continuous deployment) dans les paramètres de l'application web sur Azure en sélectionnant GitHub comme centre de déploiement :
+<br>
+<br>1. Toujours dans les paramètres de l'application web sur Azure, cliquez sur l'onglet "Déploiement continu".
+<br>2. Cliquez sur "GitHub" comme centre de déploiement et connectez-vous à votre compte GitHub si nécessaire.
+<br>3. Sélectionnez le référentiel que vous avez créé précédemment et choisissez la branche que vous souhaitez déployer.
+<br>4. Cliquez sur "Enregistrer" pour activer la fonctionnalité de déploiement continu.
 	</p>
       </div>
       <div class='tab tab-4'>
         <h1> Vérification de votre application Web :</h1>
         <p>.
-       
-
-Accédez à votre application Web PHP via un navigateur Web pour vérifier que tout fonctionne correctement.
-Vérifiez que votre base de données SQL fonctionne correctement en testant les requêtes SQL de votre application Web
+       Effectuez un commit et un push des modifications apportées à votre référentiel GitHub, et Azure récupérera automatiquement les modifications apportées à votre référentiel GitHub et les déploiera sur l'application web :
+<br>
+<br>1. Sur votre ordinateur local, apportez les modifications nécessaires à votre site web.
+<br>2. Effectuez un commit et un push des modifications apportées à votre référentiel GitHub.
+<br>3. Azure récupérera automatiquement les modifications apportées à votre référentiel GitHub et les déploiera sur l'application web. Vous pouvez suivre le processus
+		de déploiement continu en consultant les informations sur l'onglet "Déploiement continu" de votre application web sur Azure.
 	</p>
       </div>
     </div>
