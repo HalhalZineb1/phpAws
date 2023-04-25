db2b6ea9f2fb8c28999188e3ba6bf4bf18812b25
HalhalZineb1 <105636006+HalhalZineb1@users.noreply.github.com>
2023-04-26 00:22:53 +0200

    Update inscriptionn.php

--- inscriptionn.php
+++ inscriptionn.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
   <style>  body {
-  background-color: #CFE2F3;        
+    background: linear-gradient(#141e30, #243b55);       
   height: 100vh;
   display: flex;
   justify-content: center;
@@ -49,7 +49,12 @@
             </div>
         </form>
         <div>
-        <div><p>Already Registered <a href="index.php">Login Here</a></p></div>
+     
+        <div><p>Already Registered <a href="index.php"> <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+            Login Here</a></p></div>
       </div>
     </div>
 </body>
