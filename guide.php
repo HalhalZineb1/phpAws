8c405326f04a15486097a0018748c03855d84dab
HalhalZineb1 <105636006+HalhalZineb1@users.noreply.github.com>
2023-04-26 21:48:05 +0200

    Update guide.php

--- guide.php
+++ guide.php
@@ -75,6 +75,53 @@
   </style>
 </head>
 <body>
+	<!DOCTYPE html>
+<!-- indique qu'il s'agit d'une page web html -->
+<html lang="fr" >
+<!-- permet au navigateur de se préparer a lire du code html  -->
+<head>
+  <!-- informations d'entête utilisées -->
+  <meta charset="utf-8">
+  <!-- encodage de caractère -->
+  <title>Accueil</title>
+  <!-- Titre de la page -->
+  <link rel="stylesheet" href="./style.css">
+  <!-- on ajoute le style.css à la page-->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+
+  <link rel="icon" type="image/png" href="/votre-favicon.png">
+
+
+
+  <!-- on ajoute les fonts google -->
+  <script type="text/javascript" src="./js/cg_script.js"></script>
+   <style>
+      /* Styles pour le slogan */
+
+
+ 
+
+
+</style>
+
+   <link rel="icon" type="image/png" href="your-favicon.png">
+</head>
+<!-- début du corps du document html -->
+<body>
+  
+
+     <nav id="sidebarMenu" >
+	
+    <a href="accueil.php">Accueil</a>
+    <a href="guide.php">Guide déploiement</a>
+    <a href="tst1/nouveau12.php">Avantage<br> Azure.</a>
+    <a href="dist/react.php">Avis utilisateur</a>
+  </nav>
+
+  <section id="container_principal" class="container_accueil">
+    <!-- container principal avec une classe unique et un id commun à toutes les pages  -->
+    <button class="menu" onclick="ouvertureFermetureMenu()">☰</button>
+    <!-- Bouton d'ouverture du menu -->
 <img src=".\votre-favicon.png" alt="Logo du site" class="logo" width="200" height="150" style="border-radius: 50%;">
 
 <br><br>
